f1f8e260d3b0d305082f04e113a18614b8115954
Francesco Mazzoli <f@mazzo.li>
2014-12-24 15:02:00 +0100

    Use a dedicated Transform type...
    ...to represent positions and rotations.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate sdl2;
 extern crate sdl2_image;
 
 use sdl2::pixels::Color;
-use sdl2::rect::{Rect, Point};
+use sdl2::rect::Rect;
 use sdl2::SdlResult;
 use sdl2::render::{Renderer, Texture};
 use std::num::FloatMath;
@@ -29,20 +29,19 @@ fn from_radians(x: f64) -> f64 {
     x * 180./std::f64::consts::PI
 }
 
-
 // ---------------------------------------------------------------------
 // Sprites
 
 #[deriving(PartialEq, Clone, Copy)]
-struct Sprite<'tx> {
-    texture: &'tx Texture,
+struct Sprite<'a> {
+    texture: &'a Texture,
     rect: Rect,
     center: Vec2<i32>,
     // If the sprite is already rotated by some angle
     angle: f64,
 }
 
-// impl<'tx> std::fmt::Show for Sprite<'tx> {
+// impl<'a> std::fmt::Show for Sprite<'a> {
 //     fn fmt(&self, fmter: &mut std::fmt::Formatter) -> std::fmt::Result {
 //         match fmter.write_str("<<Sprite>>") {
 //             Ok(()) => Ok(()),
@@ -51,20 +50,76 @@ struct Sprite<'tx> {
 //     }
 // }
 
-impl<'tx> Sprite<'tx> {
-    fn render(&self, renderer: &Renderer, rotation: f64, pos: Vec2<i32>) -> SdlResult<()> {
+impl<'a> Sprite<'a> {
+    fn render(&self, renderer: &Renderer, trans: &Transform) -> SdlResult<()> {
         let dst = Rect{
-            x: pos.x - self.center.x,
-            y: pos.y - self.center.y,
+            x: trans.pos.x - self.center.x,
+            y: trans.pos.y - self.center.y,
             w: self.rect.w,
             h: self.rect.h
         };
-        let angle = from_radians(rotation);
+        let angle = from_radians(trans.rotation);
         renderer.copy_ex(
             self.texture, Some(self.rect), Some(dst), self.angle - angle,
             Some(self.center.point()), sdl2::render::RendererFlip::None)
     }
 }
+
+// ---------------------------------------------------------------------
+// Boxes
+
+#[deriving(PartialEq, Clone, Copy)]
+struct Transform {
+    pos: Vec2<i32>,
+    rotation: f64,
+}
+
+impl Add<Vec2<i32>, Transform> for Transform {
+    fn add(self, other: Vec2<i32>) -> Transform {
+        Transform {
+            pos: self.pos + other,
+            rotation: self.rotation
+        }
+    }
+}
+
+impl Sub<Vec2<i32>, Transform> for Transform {
+    fn sub(self, other: Vec2<i32>) -> Transform {
+        Transform {
+            pos: self.pos - other,
+            rotation: self.rotation
+        }
+    }
+}
+
+// // A convex polygon.
+// struct Poly {
+//     points: Vec<Vec2<i32>>,
+// }
+
+// struct Polys {
+//     polys: Vec<Poly>,
+// }
+
+// impl Poly {
+//     fn hits(&self, self_t: &Transform, other: &Poly, other_t: &Transform) -> bool {
+//         true
+//     }
+// }
+
+// impl Polys {
+//     fn hits(&self, self_t: &Transform, others: &Polys, others_t: &Transform) -> bool {
+//         let mut hit = false;
+//         for this in self.polys.iter() {
+//             if hit { break };
+//             for other in others.polys.iter() {
+//                 if hit { break };
+//                 hit = this.hits(self_t, other, others_t);
+//             }
+//         }
+//         return hit;
+//     }
+// }
 
 // ---------------------------------------------------------------------
 // Ship
@@ -77,29 +132,28 @@ enum Rotating {
 }
 
 #[deriving(PartialEq, Clone, Copy)]
-struct ShipSpec<'tx> {
+struct ShipSpec<'a> {
     rotation_speed: f64,
     rotation_speed_accelerating: f64,
     acceleration: f64,
     friction: f64,
     gravity: f64,
-    sprite: Sprite<'tx>,
-    sprite_accelerating: Sprite<'tx>,
-    bullet_spec: BulletSpec<'tx>,
+    sprite: &'a Sprite<'a>,
+    sprite_accelerating: &'a Sprite<'a>,
+    bullet_spec: &'a BulletSpec<'a>,
     firing_interval: u32,
     shoot_from: Vec2<i32>,
 }
 
 #[deriving(PartialEq, Clone)]
-struct Ship<'tx> {
-    spec: ShipSpec<'tx>,
-    pos: Vec2<i32>,
+struct Ship<'a> {
+    spec: &'a ShipSpec<'a>,
+    trans: Transform,
     speed: Vec2<f64>,
-    rotation: f64,
-    bullets: Vec<Bullet<'tx>>,
+    bullets: Vec<Bullet<'a>>,
 }
 
-impl<'tx> Ship<'tx> {
+impl<'a> Ship<'a> {
     fn advance(&mut self, map: &Map, accelerating: bool, firing: bool, rotating: Rotating, dt: f64) -> () {
         // =============================================================
         // Apply the rotation
@@ -111,8 +165,8 @@ impl<'tx> Ship<'tx> {
         let rotation_delta = dt * rotation_speed;
         match rotating {
             Rotating::Still => {},
-            Rotating::Left  => self.rotation += rotation_delta,
-            Rotating::Right => self.rotation -= rotation_delta,
+            Rotating::Left  => self.trans.rotation += rotation_delta,
+            Rotating::Right => self.trans.rotation -= rotation_delta,
         }
 
         // =============================================================
@@ -120,10 +174,10 @@ impl<'tx> Ship<'tx> {
         let mut f = Vec2 {x : 0., y: 0.};
         // Acceleration
         if accelerating {
-            f.x += self.rotation.cos() * self.spec.acceleration;
+            f.x += self.trans.rotation.cos() * self.spec.acceleration;
             // The sin is inverted because we push the opposite
             // direction we're looking at.
-            f.y += -1. * self.rotation.sin() * self.spec.acceleration;
+            f.y += -1. * self.trans.rotation.sin() * self.spec.acceleration;
         }
 
         // Gravity
@@ -137,9 +191,9 @@ impl<'tx> Ship<'tx> {
         self.speed = self.speed + f;
 
         // Update position
-        self.pos.x += (self.speed.x * dt) as i32;
-        self.pos.y += (self.speed.y * dt) as i32;
-        self.pos = map.bound(self.pos);
+        self.trans.pos.x += (self.speed.x * dt) as i32;
+        self.trans.pos.y += (self.speed.y * dt) as i32;
+        self.trans.pos = map.bound(self.trans.pos);
 
         // =============================================================
         // Advance the bullets
@@ -148,11 +202,10 @@ impl<'tx> Ship<'tx> {
         // =============================================================
         // Add new bullet
         if firing {
-            let shoot_from = self.spec.shoot_from.rotate_i32(Vec2{x: 0, y: 0}, self.rotation);
+            let shoot_from = self.spec.shoot_from.rotate_i32(Vec2{x: 0, y: 0}, self.trans.rotation);
             let bullet = Bullet {
                 spec: self.spec.bullet_spec,
-                pos: self.pos + shoot_from,
-                rotation: self.rotation,
+                trans: self.trans + shoot_from,
                 age: 0.,
             };
             self.bullets.push(bullet);
@@ -162,11 +215,11 @@ impl<'tx> Ship<'tx> {
     fn render(&self, renderer: &Renderer, accelerating: bool, cam: &Camera) -> () {
         // =============================================================
         // Render ship
-        let pos = cam.adjust(self.pos);
+        let trans = cam.adjust(&self.trans);
         if accelerating {
-            self.spec.sprite_accelerating.render(renderer, self.rotation, pos).ok().unwrap()
+            self.spec.sprite_accelerating.render(renderer, &trans).ok().unwrap()
         } else {
-            self.spec.sprite.render(renderer, self.rotation, pos).ok().unwrap()
+            self.spec.sprite.render(renderer, &trans).ok().unwrap()
         }
 
         // =============================================================
@@ -181,30 +234,33 @@ impl<'tx> Ship<'tx> {
 // Bullets
 
 #[deriving(PartialEq, Clone, Copy)]
-struct BulletSpec<'tx> {
-    sprite: Sprite<'tx>,
+struct BulletSpec<'a> {
+    sprite: &'a Sprite<'a>,
     speed: f64,
     lifetime: f64,
 }
 
 #[deriving(PartialEq, Clone, Copy)]
-struct Bullet<'tx> {
-    spec: BulletSpec<'tx>,
-    pos: Vec2<i32>,
-    rotation: f64,
+struct Bullet<'a> {
+    spec: &'a BulletSpec<'a>,
+    trans: Transform,
     age: f64,
 }
 
-impl<'tx> Bullet<'tx> {
-    fn advance(&self, dt: f64) -> Bullet<'tx> {
+impl<'a> Bullet<'a> {
+    fn advance(&self, dt: f64) -> Bullet<'a> {
         let pos = Vec2 {
-            x: self.pos.x + ((self.spec.speed * self.rotation.cos() * dt) as i32),
-            y: self.pos.y + ((-1. * self.spec.speed * self.rotation.sin() * dt) as i32),
+            x: self.trans.pos.x + ((self.spec.speed * self.trans.rotation.cos() * dt) as i32),
+            y: self.trans.pos.y + ((-1. * self.spec.speed * self.trans.rotation.sin() * dt) as i32),
         };
-        Bullet {pos: pos, rotation: self.rotation, age: self.age + dt, spec: self.spec}
+        Bullet {
+            trans: Transform{pos: pos, rotation: self.trans.rotation},
+            age: self.age + dt,
+            spec: self.spec,
+        }
     }
 
-    fn advance_bullets(bullets: &Vec<Bullet<'tx>>, map: &Map, dt: f64) -> Vec<Bullet<'tx>> {
+    fn advance_bullets(bullets: &Vec<Bullet<'a>>, map: &Map, dt: f64) -> Vec<Bullet<'a>> {
         let mut new_bullets = Vec::with_capacity(bullets.len() + 1);
         for bullet in bullets.iter() {
             let new_bullet = bullet.advance(dt);
@@ -216,35 +272,33 @@ impl<'tx> Bullet<'tx> {
     }
 
     fn alive(&self, map: &Map) -> bool {
-        self.pos.x >= 0 && self.pos.x <= map.w &&
-            self.pos.y >= 0 && self.pos.y <= map.h &&
+        self.trans.pos.x >= 0 && self.trans.pos.x <= map.w &&
+            self.trans.pos.y >= 0 && self.trans.pos.y <= map.h &&
             self.age < self.spec.lifetime
     }
 
     fn render(&self, renderer: &Renderer, cam: &Camera) -> () {
-        let pos = cam.adjust(self.pos);
-        self.spec.sprite.render(renderer, self.rotation, pos).ok().unwrap()
+        self.spec.sprite.render(renderer, &cam.adjust(&self.trans)).ok().unwrap()
     }
 }
 
 // ---------------------------------------------------------------------
 // Shooter
 
-struct ShooterSpec<'tx> {
-    sprite: Sprite<'tx>,
-    pos: Vec2<i32>,
-    rotation: f64,
-    bullet_spec: BulletSpec<'tx>,
+struct ShooterSpec<'a> {
+    sprite: &'a Sprite<'a>,
+    trans: Transform,
+    bullet_spec: &'a BulletSpec<'a>,
     firing_rate: f64,
 }
 
-struct Shooter<'tx> {
-    spec: ShooterSpec<'tx>,
+struct Shooter<'a> {
+    spec: &'a ShooterSpec<'a>,
     time_since_fire: f64,
-    bullets: Vec<Bullet<'tx>>,
+    bullets: Vec<Bullet<'a>>,
 }
 
-impl<'tx> Shooter<'tx> {
+impl<'a> Shooter<'a> {
     fn advance(&mut self, map: &Map, dt: f64) {
         self.bullets = Bullet::advance_bullets(&self.bullets, map, dt);
         self.time_since_fire += dt;
@@ -252,8 +306,7 @@ impl<'tx> Shooter<'tx> {
             self.time_since_fire = 0.;
             let bullet = Bullet {
                 spec: self.spec.bullet_spec,
-                pos: self.spec.pos,
-                rotation: self.spec.rotation,
+                trans: self.spec.trans,
                 age: 0.,
             };
             self.bullets.push(bullet);
@@ -261,8 +314,7 @@ impl<'tx> Shooter<'tx> {
     }
 
     fn render(&self, renderer: &Renderer, cam: &Camera) -> () {
-        let pos = cam.adjust(self.spec.pos);
-        self.spec.sprite.render(renderer, self.spec.rotation, pos).ok().unwrap();
+        self.spec.sprite.render(renderer, &cam.adjust(&self.spec.trans)).ok().unwrap();
         for bullet in self.bullets.iter() {
             bullet.render(renderer, cam);
         }
@@ -273,14 +325,14 @@ impl<'tx> Shooter<'tx> {
 // Maps
 
 #[deriving(PartialEq, Clone, Copy)]
-struct Map<'tx> {
+struct Map<'a> {
     w: i32,
     h: i32,
     background_color: Color, 
-    background_texture: &'tx Texture,
+    background_texture: &'a Texture,
 }
 
-impl<'tx> Map<'tx> {
+impl<'a> Map<'a> {
     fn render(&self, renderer: &Renderer, cam: &Camera) -> () {
         // Fill the whole screen with the background color
         renderer.set_draw_color(self.background_color).ok().unwrap();
@@ -384,8 +436,8 @@ struct Camera {
 }
 
 impl Camera {
-    fn adjust(&self, p: Vec2<i32>) -> Vec2<i32> {
-        p - self.pos
+    fn adjust(&self, trans: &Transform) -> Transform {
+        *trans - self.pos
     }
 
     #[inline(always)]
@@ -405,15 +457,15 @@ impl Camera {
         self.pos.y += (f.y * dt) as i32;
 
         // Make sure the ship is not too much to the edge
-        if self.left() + self.spec.h_padding > ship.pos.x {
-            self.pos.x = ship.pos.x - self.spec.h_padding
-        } else if self.right() - self.spec.h_padding < ship.pos.x {
-            self.pos.x = (ship.pos.x + self.spec.h_padding) - SCREEN_WIDTH
+        if self.left() + self.spec.h_padding > ship.trans.pos.x {
+            self.pos.x = ship.trans.pos.x - self.spec.h_padding
+        } else if self.right() - self.spec.h_padding < ship.trans.pos.x {
+            self.pos.x = (ship.trans.pos.x + self.spec.h_padding) - SCREEN_WIDTH
         }
-        if self.top() + self.spec.v_padding > ship.pos.y {
-            self.pos.y = ship.pos.y - self.spec.v_padding
-        } else if self.bottom() - self.spec.v_padding < ship.pos.y {
-            self.pos.y = (ship.pos.y + self.spec.v_padding) - SCREEN_HEIGHT
+        if self.top() + self.spec.v_padding > ship.trans.pos.y {
+            self.pos.y = ship.trans.pos.y - self.spec.v_padding
+        } else if self.bottom() - self.spec.v_padding < ship.trans.pos.y {
+            self.pos.y = (ship.trans.pos.y + self.spec.v_padding) - SCREEN_HEIGHT
         }
 
         // Make sure it stays in the map
@@ -421,19 +473,19 @@ impl Camera {
     }
 }
 
-struct State<'tx> {
+struct State<'a> {
     quit: bool,
     accelerating: bool,
     last_fired: Option<u32>,
     firing: bool,
     rotating: Rotating,
-    ship: Ship<'tx>,
-    map: Map<'tx>,
+    ship: Ship<'a>,
+    map: &'a Map<'a>,
     camera: Camera,
-    shooters: Vec<Shooter<'tx>>,
+    shooters: Vec<Shooter<'a>>,
 }
 
-impl<'tx> State<'tx> {
+impl<'a> State<'a> {
     fn process_events(&mut self) {
         loop {
             match sdl2::event::poll_event() {
@@ -486,11 +538,11 @@ impl<'tx> State<'tx> {
                 false,
         };
 
-        self.ship.advance(&self.map, self.accelerating, firing, self.rotating, dt);
+        self.ship.advance(self.map, self.accelerating, firing, self.rotating, dt);
         for i in range(0, self.shooters.len()) {
-            self.shooters[i].advance(&self.map, dt);
+            self.shooters[i].advance(self.map, dt);
         }
-        self.camera.advance(&self.map, &self.ship, dt);
+        self.camera.advance(self.map, &self.ship, dt);
     }
 
     fn render(&self, renderer: &Renderer) {
@@ -536,11 +588,11 @@ fn main() {
         sdl2::render::RenderDriverIndex::Auto,
         sdl2::render::ACCELERATED | sdl2::render::PRESENTVSYNC).ok().unwrap();
     renderer.set_logical_size((SCREEN_WIDTH as int), (SCREEN_HEIGHT as int)).ok().unwrap();
-    let planes_surface = sdl2_image::LoadSurface::from_file(&from_str("assets/planes.png").unwrap()).ok().unwrap();
+    let planes_surface = sdl2_image::LoadSurface::from_file(&("assets/planes.png".parse()).unwrap()).ok().unwrap();
     planes_surface.set_color_key(true, Color::RGB(0xba, 0xfe, 0xca)).ok().unwrap();
     let planes_texture: &Texture = &renderer.create_texture_from_surface(&planes_surface).ok().unwrap();
-    let bullet_spec = BulletSpec {
-        sprite: Sprite {
+    let bullet_spec = &BulletSpec {
+        sprite: &Sprite {
             texture: planes_texture,
             rect: Rect{x: 424, y: 140, w: 3, h: 12},
             center: Vec2{x: 1, y: 6},
@@ -551,19 +603,19 @@ fn main() {
     };
     let ship_pos = Vec2 {x: SCREEN_WIDTH / 2, y: SCREEN_HEIGHT / 2};
     let ship = Ship {
-        spec : ShipSpec {
-            rotation_speed: 0.007,
-            rotation_speed_accelerating: 0.002,
-            acceleration: 0.035,
+        spec: &ShipSpec {
+            rotation_speed: 0.01,
+            rotation_speed_accelerating: 0.001,
+            acceleration: 0.025,
             friction: 0.02,
             gravity: 0.008,
-            sprite: Sprite{
+            sprite: &Sprite{
                 texture: planes_texture,
                 rect: Rect{x: 128, y: 96, w: 30, h: 24},
                 center: Vec2{x: 15, y: 12},
                 angle: 90.,
             },
-            sprite_accelerating: Sprite {
+            sprite_accelerating: &Sprite {
                 texture: planes_texture,
                 rect: Rect{x: 88, y: 96, w: 30, h: 24},
                 center: Vec2{x: 15, y: 12},
@@ -573,18 +625,34 @@ fn main() {
             firing_interval: 1000,
             shoot_from: Vec2{x: 18, y: 0},
         },
-        pos: ship_pos,
+        trans: Transform {
+            pos: ship_pos,
+            rotation: 0.,
+        },
         speed: Vec2 {x: 0., y: 0.},
-        rotation: 0.,
         bullets: Vec::new(),
     };
-    let map_surface = sdl2_image::LoadSurface::from_file(&from_str("assets/background.png").unwrap()).ok().unwrap();
+    let map_surface = sdl2_image::LoadSurface::from_file(&("assets/background.png".parse()).unwrap()).ok().unwrap();
     let map_texture = renderer.create_texture_from_surface(&map_surface).ok().unwrap();
-    let map = Map {
+    let map = &Map {
         w: SCREEN_WIDTH*10,
         h: SCREEN_HEIGHT*10,
         background_color: Color::RGB(0x58, 0xB7, 0xFF),
         background_texture: &map_texture,
+    };
+    let shooter_spec = &ShooterSpec {
+        sprite: &Sprite {
+            texture: planes_texture,
+            rect: Rect{x: 48, y: 248, w: 32, h: 24},
+            center: Vec2{x: 16, y: 12},
+            angle: 90.,
+        },
+        trans: Transform {
+            pos: Vec2{x: 1000, y: 200},
+            rotation: to_radians(270.),
+        },
+        bullet_spec: bullet_spec,
+        firing_rate: 2000.,
     };
     let mut state = State {
         quit: false,
@@ -607,18 +675,7 @@ fn main() {
         },
         shooters: vec![
             Shooter {
-                spec: ShooterSpec {
-                    sprite: Sprite {
-                        texture: planes_texture,
-                        rect: Rect{x: 48, y: 248, w: 32, h: 24},
-                        center: Vec2{x: 16, y: 12},
-                        angle: 90.,
-                    },
-                    pos: Vec2{x: 1000, y: 200},
-                    rotation: to_radians(90.),
-                    bullet_spec: bullet_spec,
-                    firing_rate: 2000.,
-                },
+                spec: shooter_spec,
                 time_since_fire: 0.,
                 bullets: Vec::new(),
             }
